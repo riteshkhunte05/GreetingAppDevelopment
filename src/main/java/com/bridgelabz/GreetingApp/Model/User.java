7843dd0e115d95218cac1da6f03c09e2941c96ca
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-31 16:29:50 +0530

    Added: Using Greeting Controller return Json for different HTTP methods

---
+++ src/main/java/com/bridgelabz/GreetingApp/Model/User.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.GreetingApp.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class User {
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    private String lastName;
+
+    @Override
+    public String toString() {
+        String str = (firstName != null) ? firstName + " " : "";
+        str += (lastName != null) ? lastName : "";
+        return str.trim();
+    }
+}
